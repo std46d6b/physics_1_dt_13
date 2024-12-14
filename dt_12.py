3d3d286cf01e1132e2b98f523428d3453a133098
std46d6b <dev@m5k.ru>
2024-12-14 23:24:06 +0300

    v1 - app improved

--- dt_12.py
+++ dt_12.py
@@ -1,6 +1,14 @@
+import logging
+
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
+
+log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.ERROR, format=log_format)
+
+log = logging.getLogger(__name__)
+logging.getLogger(__name__).setLevel(logging.INFO)
 
 
 class PointCharge:
@@ -54,49 +62,70 @@ def potential(charges, x, y):
 
 print("Введите параметры для моделирования электростатического поля")
 
-q1 = float(input("Значение заряда 1 (пример: 0.02 Кл): ") or 0.02)
-x1 = float(input("X-координату заряда 1 (пример: -2): ") or -2)
-y1 = float(input("Y-координату заряда 1 (пример: -1): ") or -1)
+log.debug("Ожидание ввода параметров")
+q1 = float(input("Значение заряда 1 (пример:  0.02 Кл): ") or 0.02)
+x1 = float(input("X-координату заряда 1   (пример: -2): ") or -20)
+y1 = float(input("Y-координату заряда 1   (пример: -1): ") or -10)
 
-q2 = float(input("Значение заряда 2 (пример: -0.02 Кл): ") or -0.02)
-x2 = float(input("X-координату заряда 2 (пример: 2): ") or 2)
-y2 = float(input("Y-координату заряда 2 (пример: 1): ") or 1)
+q2 = float(input("Значение заряда 2 (пример: -0.01 Кл): ") or -0.01)
+x2 = float(input("X-координату заряда 2    (пример: 2): ") or 20)
+y2 = float(input("Y-координату заряда 2    (пример: 1): ") or 10)
+log.debug("Параметры введены")
+
+log.debug(f"Заряд 1: {q1} Кл, X-координата: {x1}, Y-координата: {y1}")
+log.debug(f"Заряд 2: {q2} Кл, X-координата: {x2}, Y-координата: {y2}")
+
+log.info("Начало вычислений")
 
 charges = [
     PointCharge(q1, x1, y1),
     PointCharge(q2, x2, y2)
 ]
 
-linspace_r = 10
+linspace_r = int(np.sqrt(abs(x1 - x2) ** 2 + abs(y1 - y2) ** 2 + q2 ** 2) * 1.1 + 1)
 grids = 500
-x = np.linspace(-linspace_r, linspace_r, grids)
-y = np.linspace(-linspace_r, linspace_r, grids)
+x_mid = (x2 + x1) / 2
+y_mid = (y2 + y1) / 2
+
+log.info("Генерация сетки координат")
+x = np.linspace(-linspace_r + x_mid, linspace_r + x_mid, grids)
+y = np.linspace(-linspace_r + y_mid, linspace_r + y_mid, grids)
 X, Y = np.meshgrid(x, y)
 
 Ex, Ey = np.zeros(X.shape), np.zeros(Y.shape)
 V = np.zeros(X.shape)
 
+log.info("Вычисление компонентов поля и потенциала")
 for i in range(X.shape[0]):
     for j in range(X.shape[1]):
         Ex[i, j], Ey[i, j] = electric_field(charges, X[i, j], Y[i, j])
         V[i, j] = potential(charges, X[i, j], Y[i, j])
 
+log.info("Построение графика")
 plt.figure(figsize=(10, 10))
 colors = np.sqrt(Ex ** 2 + Ey ** 2)
 plt.streamplot(X, Y, Ex, Ey, color=colors, linewidth=1, density=2.5, cmap=cm.rainbow, arrowstyle='->', arrowsize=1.5)
 
-contour_levels = np.linspace(np.min(V), np.max(V), 50)
+log.info("Построение контурных линий потенциала")
+vv = np.sort(np.unique(V))
+log.debug(f"Число уникальных значений потенциала: {len(vv)} / {len(V)}")
+vn, vp = vv[vv < 0], vv[vv > 0]
+
+log.debug(f"Интервалы потенциала: {vn[0]}, {vp[-1]}")
+
+amp = 0.1
+contour_levels = np.linspace(vn[int(len(vn) / 100 * amp)], vp[int(len(vp) / 100 * (100 - amp))], 250)
+
 plt.contour(X, Y, V, levels=contour_levels, cmap=cm.viridis, alpha=0.75)
 
 for charge in charges:
-    if charge.q > 0:
-        plt.plot(charge.x, charge.y, 'ro', markersize=15)
-    else:
-        plt.plot(charge.x, charge.y, 'bo', markersize=15)
+    plt.plot(charge.x, charge.y, 'ro' if charge.q > 0 else 'bo', markersize=15)
 
+log.info("Настройка графика")
 plt.xlabel('X')
 plt.ylabel('Y', rotation=0)
 plt.title('Электростатическое поле и эквипотенциальные поверхности')
 plt.grid()
 plt.axis('equal')
+log.info("Вывод графика")
 plt.show()
